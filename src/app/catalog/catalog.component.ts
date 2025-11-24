c3426fd05db12f448c573e07903b73c6a6ab8b99
Mario Madrigal <mmadrigalac@gmail.com>
2025-11-24 10:55:32 -0600

    Added application routing module

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product.model';
-import { CartService } from '../cart.service';
+import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -14,17 +15,24 @@ export class CatalogComponent {
 
   constructor(
     private cartSrv: CartService,
-    private productSrv: ProductService
+    private productSrv: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
     this.productSrv.getProducts().subscribe((products) => {
       this.products = products;
     });
+
+    this.route.queryParams.subscribe((params) => {
+      this.filter = params['filter'] ?? '';
+    });
   }
 
   addToCart(product: IProduct) {
     this.cartSrv.add(product);
+    this.router.navigate(['/cart']);
   }
 
   getFilteredProducts() {
